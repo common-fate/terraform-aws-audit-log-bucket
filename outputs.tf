fd427b4a5bbe9265cdc8aa8f5acc5a2b8e4c0edf
chrnorm <17420369+chrnorm@users.noreply.github.com>
2024-05-28 11:39:38 +0100

    add descriptions to outputs

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,14 @@
 output "bucket_name" {
-  value = aws_s3_bucket.this.bucket
+  description = "The name of the created S3 bucket."
+  value       = aws_s3_bucket.this.bucket
 }
 
 output "bucket_arn" {
-  value = aws_s3_bucket.this.arn
+  description = "The ARN of the created S3 bucket."
+  value       = aws_s3_bucket.this.arn
 }
 
 output "write_role_arn" {
-  value = aws_iam_role.write.arn
+  description = "The ARN of the role created with permission to write to the S3 bucket."
+  value       = aws_iam_role.write.arn
 }
